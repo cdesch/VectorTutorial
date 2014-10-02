3244828612ec3dc7ad4393e41b811a9598e8b334
Chris <cdesch@gmail.com>
2014-10-02 10:16:20 -0400

    Initial Commit

---
+++ VectorTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VectorTutorial
+//
+//  Created by cj on 10/2/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
